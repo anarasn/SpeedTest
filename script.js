2d216eb8254ffb727c848b0acb0fdbb7f1e99b39
Test User <luner-stone@mail.ru>
2022-05-23 17:41:00 +0300

    I init

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-const 
+const word = document.getElementById("word")
+const text = document.querySelector("#text")
+const scoreEl = document.querySelector("#score")
+const timeEl = document.querySelector("#time")
+const endGameEl = document.querySelector(".end-game-container")
+const settingsBtn = document.getElementById("settings-btn")
+const settings = document.getElementById("settings")
+const settingsForm = document.getElementById("settings-form")
+const difficulty = document.getElementById("difficulty")
+const words = [
+    'sigh',
+    'tense',
+    'airplane',
+    'ball',
+    'pies',
+    'juice',
+    'warlike',
+    'bad',
+    'north',
+    'dependent',
+    'steer',
+    'silver',
+    'highfalutin',
+    'superficial',
+    'quince',
+    'eight',
+    'feeble',
+    'admit',
+    'drag',
+    'loving'
+  ];
+
+//init word
+let randomWord 
+
+//init score
+let score = 0
+
+//init time
+let time = 10
+
